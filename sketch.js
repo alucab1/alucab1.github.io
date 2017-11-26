c1d64998cdfb89cf2c23e72e001b6150cc1b74e9
Alex Cabrera 19 <AlexCabrera19@newmanschool.org>
2017-11-26 13:31:59 -0600

    final product

--- sketch.js
+++ sketch.js
@@ -1,8 +1,6 @@
 //global variables
-var player = {statPoints: 20, att: 15, def: 13, hp: 30, luck: 5, speed:10};
-var playerName = prompt("Enter A Name", "Player");
+var player = {name: "Player", statPoints: 20, att: 15, def: 13, hp: 30, speed:10};
 var exclamation;
-var startTurn;
 var turn;
 var slashSound;
 var slash;
@@ -20,34 +18,25 @@ var enemy =[];
 var enemyHitSound = [];
 var playerHealthBar;
 var enemyHealthBar;
+var newTurn;
 var turnAnnounced = false;
-// Enemy(name, att, def, hp, luck, speed)
 
+// Enemy(name, att, def, hp, luck, speed, filename)
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  enemy[0] = new Enemy("enemy1", 7, 20, 20, 10, 600, "pictures/angryvillagers.jpg");
-  enemy[1] = new Enemy("enemy2", 9, 25, 22, 12, 500, "pictures/clipart.jpg");
-  enemy[2] = new Enemy("enemy3", 12, 30, 25, 14, 300, "pictures/waluigi.png");
-  enemy[3] = new Enemy("enemy4", 15, 35, 27, 15, 250, "pictures/weeb.jpeg");
-  enemy[4] = new Enemy("enemy4", 20, 50, 65, 17, 190, "pictures/guile.png");
+  enemy[0] = new Enemy("A mob of angry villagers", 7, 20, 20, 600, "pictures/angryvillagers.jpg");
+  enemy[1] = new Enemy("An average badguy", 9, 30, 22, 500, "pictures/clipart.jpg");
+  enemy[2] = new Enemy("Waluigi", 12, 40, 24, 300, "pictures/waluigi.png");
+  enemy[3] = new Enemy("A real ninja", 15, 50, 26, 250, "pictures/weeb.jpeg");
+  enemy[4] = new Enemy("Guile", 20, 50, 50, 190, "pictures/guile.png");
   for (i = 0; i < enemy.length; i++){
     enemy[i].sprite.hide();
   }
-
+  player.name = prompt("Enter A Name", "Player");
   raiseStat("Attack", player.att, 1);
   raiseStat("Defence", player.def, 4);
   raiseStat("HP", player.hp, 2);
-  // raiseAttack();
-  // console.log("Attack = "+player.att);
-  // raiseDefence();
-  // console.log("Defence = "+player.def);
-  // raiseHP();
   maxPlayerHP = player.hp;
-  // console.log("HP = "+player.hp);
-  // // raiseLuck();
-  // // console.log("Luck = "+player.luck);
-  // raiseSpeed();
-  // console.log("Speed = "+player.speed);
 
   playerHealthBar = createImg("pictures/red.png");
   playerHealthBar.position(108, 58);
@@ -61,26 +50,7 @@ function setup(){
 }
 
  function preload(){
-//   // exclamation = loadImage("capture.PNG");
-//   if (!loadImage("pictures/capture.PNG")){
-//     console.log("Error");
-//   }
-//   else {
-//     console.log("OK");
-//   }
-//   if (!loadImage("pictures/slash.png")){
-//     console.log("Error");
-//   }
-//   else {
-//     console.log("OK");
-//   }
-//   if (!loadImage("pictures/red.png")){
-//     console.log("Error");
-//   }
-//   else {
-//     console.log("OK");
-//   }
-
+  //loading soundeffects
   soundFormats("mp3", "wav");
   slashSound = loadSound("sounds/slash.wav");
   enemyHitSound[0] = loadSound("sounds/villagernoise.mp3");
@@ -89,17 +59,17 @@ function setup(){
   enemyHitSound[3] = loadSound("sounds/Omae wa.mp3");
   enemyHitSound[4] = loadSound("sounds/Sonic Boom.mp3");
 }
-//constructor
-function Enemy(name, att, def, hp, luck, speed, spriteFile, songFile){
+//obect constructor for enemies
+function Enemy(name, att, def, hp, speed, spriteFile, songFile){
   this._name = name;
   this.att = att;
   this.def = def;
   this.hp = hp;
   this.maxHP = hp;
-  this.luck = luck;
   this.speed = speed;
   this.sprite = createImg(spriteFile);
-  this.sprite.position(486-this.sprite.width, 255-this.sprite.height);
+  this.sprite.size(500, 475)
+  this.sprite.position(windowWidth/2-this.sprite.width/2, windowHeight/2-this.sprite.height/2+30);
   this.turnAnnounce = function(){
     if (turnAnnounced == false);
     text(this._name+" Has appeared", 100, 100, 100, 100);
@@ -121,79 +91,29 @@ function Enemy(name, att, def, hp, luck, speed, spriteFile, songFile){
   this.timer = function(){
     whatTime = "late";
     player.hp-=this.att - (this.att*player.def/100);
-    //enemyHitSound[enemyNumber].play();
-    console.log(playerName +"'s hp = " + player.hp);
+    console.log(player.name +"'s hp = " + player.hp);
     if (this.hp > 0 && player.hp > 0){
       restartTurn();
     }
     else if(player.hp <= 0){
       gameOverInitiate();
     }
-    else if (this.hp <= 0){
-      background(255);
-      console.log(playerName +" defeated " + enemy[enemyNumber]._name);
-      enemy[enemyNumber].sprite.hide();
-      enemyNumber++;
-
-      player.statpoints+=5;
-      raiseAttack();
-      console.log("Attack = "+player.att);
-      raiseDefence();
-      console.log("Defence = "+player.def);
-      raiseHP();
-      maxPlayerHP = player.hp;
-      console.log("HP = "+player.hp);
-      raiseSpeed();
-      console.log("Speed = "+player.speed);
-
-      restartTurn();
-    }
   }.bind(this)
 }
 
+//will call the attackTurn method within a partially random amount of time
 function turnPrep(i){
-  enemy[i].sprite.show();
-  enemy[i].turnAnnounce();
-  whatTime = "Miss";
-  if (enemy[enemyNumber].hp > 0 && player.hp > 0){
-    turn = setTimeout(enemy[enemyNumber].attackTurn, floor(random(4500))+2400);
-  }
-  //Game Over
-  // else if(player.hp <= 0 || strikes == 3){
-  //   background(0);
-  //   playerHealthBar.hide();
-  //   enemyHealthBar.hide();
-  //   enemy[enemyNumber].sprite.hide();
-  //   slash.hide();
-  //   text("Game Over", windowWidth/2, windowHeight/2);
-  //   gameOver = true;
-  // }
-  // //
-  // else if (enemy[enemyNumber].hp <= 0){
-  //   background(255);
-  //   console.log(playerName +" defeated " + enemy[enemyNumber]._name);
-  //   enemy[enemyNumber].sprite.hide();
-  //   enemyNumber++;
-  //   strikes = 0;
-  //
-  //   player.statpoints+=5;
-  //   raiseAttack();
-  //   console.log("Attack = "+player.att);
-  //   raiseDefence();
-  //   console.log("Defence = "+player.def);
-  //   raiseHP();
-  //   maxPlayerHP = player.hp;
-  //   console.log("HP = "+player.hp);
-  //   raiseSpeed();
-  //   console.log("Speed = "+player.speed);
-  //
-  //   setTimeout(turnPrep, 1000, enemyNumber);
-  // }
-  else{
-    console.log("something happened");
+  if (gameOver == false) {
+    enemy[i].sprite.show();
+    enemy[i].turnAnnounce();
+    whatTime = "Miss";
+    if (enemy[enemyNumber].hp > 0 && player.hp > 0){
+      turn = setTimeout(enemy[enemyNumber].attackTurn, floor(random(5000))+1700);
+    }
   }
 }
 
+//is called if you press a key
 function keyPressed(){
   slash = createImg("pictures/slash.png");
   slash.size(215, 175);
@@ -201,73 +121,81 @@ function keyPressed(){
   slash.show();
   setTimeout(function(){slash.hide()}, 100);
   if (whatTime == "Miss"){
-    setTimeout(function(){slash.hide()}, 100);
-    clearTimeout(turn);
-    clearTimeout(timer);
-    console.log("strikes = "+strikes);
-    strikes++;
-    if(strikes == 3){
-      gameOverInitiate();
-    }
-    else{
-      restartTurn();
-    }
+    miss();
   }
   else if (whatTime == "hit"){
-    clearTimeout(timer);
-    slashSound.play();
-    exclamation.hide();
-    enemy[enemyNumber].hp -= player.att - (player.att*enemy[enemyNumber].def/100);
-    console.log(enemy[enemyNumber]._name+"'s hp = " + enemy[enemyNumber].hp);
-    if (enemy[enemyNumber].hp > 0 && player.hp > 0){
-      restartTurn();
-    }
-    else if (enemy[enemyNumber].hp <= 0){
-      background(255);
-      console.log(playerName +" defeated " + enemy[enemyNumber]._name);
-      enemy[enemyNumber].sprite.hide();
-      enemyNumber++;
-      strikes = 0;
-
-      player.hp+=5;
-      // console.log("Attack = "+player.att);
-      // raiseDefence();
-      // console.log("Defence = "+player.def);
-      // raiseHP();
-      if (player.hp>maxPlayerHP){
-        maxPlayerHP = player.hp;
-      }
-      // console.log("HP = "+player.hp);
-      // raiseSpeed();
-      // console.log("Speed = "+player.speed);
-      turnAnnounced = false;
-      restartTurn();
-    }
-    whatTime = "Miss";
+    hit();
   }
 }
 
+//is called if you hit a key early
+function miss(){
+  clearTimeout(turn);
+  clearTimeout(timer);
+  console.log("strikes = "+strikes);
+  strikes++;
+  if(strikes == 3){
+    gameOverInitiate();
+  }
+  else{
+    restartTurn();
+  }
+}
+
+//is called if you hit a key at the correct time
+function hit(){
+  clearTimeout(timer);
+  slashSound.play();
+  exclamation.hide();
+  enemy[enemyNumber].hp -= player.att - (player.att*enemy[enemyNumber].def/100);
+  console.log(enemy[enemyNumber]._name+"'s hp = " + enemy[enemyNumber].hp);
+
+  if (enemy[enemyNumber].hp > 0 && player.hp > 0){
+    restartTurn();
+  }
+
+  else if (enemy[enemyNumber].hp <= 0){
+    background(255);
+    console.log(player.name +" defeated " + enemy[enemyNumber]._name);
+    enemy[enemyNumber].sprite.hide();
+    enemyNumber++;
+    strikes = 0;
+    var expectedInput = false;
+    while (expectedInput == false){
+      player.statPoints = 5;
+      buffChoice = prompt("Do you want to raise attack(a) or health(h)?", "a")
+      if (buffChoice == "h"){
+        player.hp+=6;
+        expectedInput = true;
+      }
+      else if (buffChoice == "a"){
+        player.att+=4;
+        expectedInput = true;
+      }
+    }
+    if (player.hp>maxPlayerHP){
+      maxPlayerHP = player.hp;
+    }
+    turnAnnounced = false;
+    restartTurn();
+  }
+  whatTime = "Miss";
+}
+
+function buffStats(){
+
+}
+//will wait one second then call the turnPrep function
 function restartTurn(){
-  setTimeout(turnPrep, 1000, enemyNumber);
+  newTurn = setTimeout(turnPrep, 1000, enemyNumber);
 }
 
 //Stat Distribution
 
-// function raiseAttack(){
-//   able = false;
-//   while(able === false) {
-//     var add= Number(prompt("How Many Points Do You Want To Add To Attack?  Current Attack = "+player.att, player.statPoints));
-//     if (add<=player.statPoints){
-//       able = true;
-//     }
-//   }
-//   player.statPoints -= add;
-//   player.att += add;
-// }
 function raiseStat(name, stat, multiplier){
   able = false;
   while(able === false) {
-    var add= Number(prompt("How Many Points Do You Want To Add To "+ name +"  Current "+ name  +" = "+stat, player.statPoints));
+    var add= Number(prompt("How Many Points Do You Want To Add To "+ name +"  Current "+ name  +" = "+stat, "points left = "player.statPoints));
     if (add<=player.statPoints){
       able = true;
     }
@@ -275,39 +203,6 @@ function raiseStat(name, stat, multiplier){
   player.statPoints -= add;
   player.def += add*multiplier;
 }
-// function raiseHP(){
-//   able = false;
-//   while(able === false) {
-//     var add= Number(prompt("How Many Points Do You Want To Add To Max HP?  Current HP = "+player.hp, player.statPoints));
-//     if (add<=player.statPoints){
-//       able =   true;
-//     }
-//   }
-//   player.statPoints -= add;
-//   player.att += add*2;
-// }
-// function raiseLuck(){
-//   able = false;
-//   while(able === false) {
-//     var add= Number(prompt("How Many Points Do You Want To Add To Luck?  Current Attack = "+player.luck, player.statPoints));
-//     if (add<=player.statPoints){
-//       able = true;
-//     }
-//   }
-//   player.statPoints -= add;
-//   player.luck += add;
-// }
-// function raiseSpeed(){
-//   able = false;
-//   while(able === false) {
-//     var add= Number(prompt("How Many Points Do You Want To Add To Speed?  Current Attack = "+player.speed, player.statPoints));
-//     if (add<=player.statPoints){
-//       able = true;
-//     }
-//   }
-//   player.statPoints -= add;
-//   player.speed += add;
-// }
 
 //TODO write console game intro
 
@@ -320,13 +215,18 @@ function draw(){
 
 function gameOverInitiate(){
   gameOver = true;
+  clearTimeout(newTurn);
+  clearTimeout(turn);
+  clearTimeout(timer);
   playerHealthBar.hide();
   enemyHealthBar.hide();
   enemy[enemyNumber].sprite.hide();
   slash.hide();
   background(0);
   text("Game Over", windowWidth/2, windowHeight/2);
-  end();
+  if (gameOver == true){
+    throw new Error();
+  }
 }
 
 function strikeDraw(){
